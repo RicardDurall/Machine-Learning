8e2d8539b7f3f46f47c4134352f12bcbc1621e05
RicardDurall <ricarddurall@hotmail.com>
2018-01-14 16:01:28 +0100

    Add files via upload

---
+++ CNN/Predict.py
@@ -0,0 +1,17 @@
+import CNN, csv
+
+
+weights = ["/home/ricard/Desktop/Kaggle/Plants/weights0.hdf5",
+           "/home/ricard/Desktop/Kaggle/Plants/weights1.hdf5",
+           "/home/ricard/Desktop/Kaggle/Plants/weights2.hdf5",
+           "/home/ricard/Desktop/Kaggle/Plants/weights3.hdf5",
+           "/home/ricard/Desktop/Kaggle/Plants/weights4.hdf5"]
+
+rows = CNN.prediction(weights)
+
+
+with open("/home/ricard/Desktop/Kaggle/Plants/output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["file", "species"])
+    for row in rows:
+        writer.writerow(row)
